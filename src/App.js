15910928b46962b47576f47a0d83abeaa0caf116
07_Itamar_Melamed <nadav8.tch07@gmail.com>
2022-08-29 14:09:46 +0300

    add pages and components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainPage from './pages/MainPage';
+import AllPage from './pages/AllPage';
+import CompletedPage from './pages/CompletedPage';
+import LeftPage from './pages/LeftPage';
 
 function App() {
   return (
@@ -8,8 +12,10 @@ function App() {
       <header className="App-header">
         <h1>Welcome to React Router!</h1>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="all" element={<AllPage />} />
+          <Route path="completed" element={<CompletedPage />} />
+          <Route path="left" element={<LeftPage />} />
         </Routes>
       </header>
     </div>
